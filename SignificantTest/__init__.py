215500a43dca0f714b8186e00794e70a5710c98a
Hoai Bach <bachbk1611@gmail.com>
2021-07-02 14:54:37 +1200

    Implement significant test

---
+++ SignificantTest/__init__.py
@@ -0,0 +1,10 @@
+"""
+Authors: Bach Nguyen
+Created: 01/07/2021
+Description:
+-------------------------------------------------------------------------------------------
+This package implements necessary significant test
+-------------------------------------------------------------------------------------------
+"""
+from SignificantTest import nonparametric_tests as non_test
+def
